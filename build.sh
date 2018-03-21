760ae0a5b6a4fb2b1f512feaaab65b975fca35b4
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-03-21 17:56:56 +0000

    Add build configuration and documentation

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "mediawiki_version=1.30.0" mediawiki.json
+MEDIAWIKI_VERSION=1.30.0
+
+${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "mediawiki_version=${MEDIAWIKI_VERSION}" mediawiki.json
