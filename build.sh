0ac711fe09670627beb3bd6e870f8dea114f66b7
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-07-04 16:23:48 +0100

    Update mediawiki to 1.31

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-MEDIAWIKI_VERSION=1.30.0
+MEDIAWIKI_VERSION=1.31.0
 
 ${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" -var "mediawiki_version=${MEDIAWIKI_VERSION}" -var "destination_regions=${DESTINATION_REGIONS}" build.json
