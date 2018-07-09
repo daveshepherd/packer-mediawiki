83c0c7468ed4174c5dcbcd13e6ec82ffbdf141cb
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-07-09 16:23:13 +0100

    Add: AWS extention for storing images in S3

--- shell/mediawiki.sh
+++ shell/mediawiki.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-until sudo apt-get -y install apache2 php php-mysql libapache2-mod-php php-xml php-mbstring php-apcu php-intl imagemagick nfs-common percona-toolkit
+until sudo apt -y install apache2 php php-mysql libapache2-mod-php php-xml php-mbstring php-apcu php-intl imagemagick nfs-common percona-toolkit composer php7.2-curl zip
 do
   echo "Try again"
   sleep 5
@@ -11,11 +11,13 @@ done
 MEDIAWIKI_MAJOR_MINOR_VERSION=$(echo ${MEDIAWIKI_VERSION} | sed -E  's/\.[[:digit:]]+$//g')
 
 curl -o /tmp/mediawiki.tar.gz https://releases.wikimedia.org/mediawiki/${MEDIAWIKI_MAJOR_MINOR_VERSION}/mediawiki-${MEDIAWIKI_VERSION}.tar.gz
-
 tar -xvzf /tmp/mediawiki.tar.gz
-
 sudo mkdir /var/lib/mediawiki
-
 sudo mv mediawiki-*/* /var/lib/mediawiki
-
 sudo ln -s /var/lib/mediawiki /var/www/html/mediawiki
+
+
+git clone --depth 1 https://github.com/edwardspec/mediawiki-aws-s3.git /tmp/AWS
+sudo mv /tmp/AWS /var/www/html/mediawiki/extensions/
+cd /var/www/html/mediawiki/extensions/AWS
+composer install
