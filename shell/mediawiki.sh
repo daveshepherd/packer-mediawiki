04a8353cd1af15cc9c2ae97a3f5f70e063e13d64
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-09-21 14:44:29 +0100

    Tidy up mediawiki installation, including the default url

--- shell/mediawiki.sh
+++ shell/mediawiki.sh
@@ -17,14 +17,33 @@ done
 
 MEDIAWIKI_MAJOR_MINOR_VERSION=$(echo ${MEDIAWIKI_VERSION} | sed -E  's/\.[[:digit:]]+$//g')
 
+# install mediawiki
 curl -o /tmp/mediawiki.tar.gz https://releases.wikimedia.org/mediawiki/${MEDIAWIKI_MAJOR_MINOR_VERSION}/mediawiki-${MEDIAWIKI_VERSION}.tar.gz
 tar -xvzf /tmp/mediawiki.tar.gz
 sudo mkdir /var/lib/mediawiki
 sudo mv mediawiki-*/* /var/lib/mediawiki
-sudo ln -s /var/lib/mediawiki /var/www/html/mediawiki
+sudo ln -s /var/lib/mediawiki /var/www/html/w
 
-
+# install AWS mediawiki extension
 git clone --depth 1 https://github.com/edwardspec/mediawiki-aws-s3.git /tmp/AWS
-sudo mv /tmp/AWS /var/www/html/mediawiki/extensions/
-cd /var/www/html/mediawiki/extensions/AWS
+sudo mv /tmp/AWS /var/lib/mediawiki/extensions/
+cd /var/lib/mediawiki/extensions/AWS
 composer install
+
+### Configure apache
+sudo sh -c 'cat << EOF > /etc/apache2/sites-available/mediawiki.conf
+<VirtualHost *:80>
+        RewriteEngine On
+        RewriteRule ^/?article(/.*)?\$ %{DOCUMENT_ROOT}/w/index.php [L]
+        RewriteRule ^/*\$ %{DOCUMENT_ROOT}/w/index.php [L]
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF'
+
+sudo a2enmod rewrite
+sudo a2dissite 000-default
+sudo a2ensite mediawiki
